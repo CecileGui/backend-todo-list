be2935b88751dfc716198fdc48cb97bbf3acd14d
Cecile Guichaoua <cecile.guichaoua@gmail.com>
2020-09-01 17:38:42 +0200

    ajout gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const Schema = mongoose.Schema();
+const router = express.Router();
+
+app.listen(4000, () => {
+    console.log("The server is up and running on port 4000")
+})
