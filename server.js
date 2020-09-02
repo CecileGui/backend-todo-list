a505b02e72f259f0bd8b17c0acfabfacc0e8968e
Cecile Guichaoua <cecile.guichaoua@gmail.com>
2020-09-02 11:50:50 +0200

    connexion Database MongoDB ok

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ const router = express.Router();
 app.listen(4000, () => {
     console.log("The server is up and running on port 4000")
 })
+
+const db = "mongodb+srv://user_a:8O5asWvipBFKaTAK1t2X@cluster0.oxdgo.mongodb.net/TodoMERN?retryWrites=true&w=majority"
+
+mongoose.connect(db)
+.then(() => console.log("Connexion sucess!"))
+.catch(err => (console.log(err)))
